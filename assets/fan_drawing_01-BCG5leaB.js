4c860883a2c9a2ebe0cf5b96de0fce5dd8f90094
Yuuta Tsubasa <yuuta.tsubasa.knight@gmail.com>
2024-09-19 10:33:20 +0800

    Updates

---
+++ assets/fan_drawing_01-BCG5leaB.js
@@ -0,0 +1,14 @@
+const n=`---
+title: "悠然翼繪 by 诗婷 2021/06/14"
+date: "2021-06-14"
+author: "诗婷"
+thumbnail: "/images/posts/fan_drawing_01.webp"
+tags: ["翼友作品"]
+excerpt: "【诗婷】繪製的粉絲繪。"
+---
+【诗婷】繪製的粉絲繪。
+
+感覺是在咬著小籠包的我，也感覺好像其實是在咬著枕頭之類的東西～XD
+
+原推文：[https://x.com/0112_ting/status/1404275813692760066](https://x.com/0112_ting/status/1404275813692760066)
+`;export{n as default};
