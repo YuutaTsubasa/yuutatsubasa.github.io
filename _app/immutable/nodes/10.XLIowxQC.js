b0a17230c88d9a02323f564099a96cb2df4c7116
YuutaTsubasa <yuuta.tsubasa.knight@gmail.com>
2024-09-23 03:33:35 +0800

    Updates

---
+++ _app/immutable/nodes/10.XLIowxQC.js
@@ -0,0 +1,1 @@
+import{s as n}from"../chunks/scheduler.D8xYWXqZ.js";import{S as s,i as e}from"../chunks/index.DWKWhCLW.js";function r(o){return window.location.href="/posts/1",[]}class c extends s{constructor(t){super(),e(this,t,r,null,n,{})}}export{c as component};
