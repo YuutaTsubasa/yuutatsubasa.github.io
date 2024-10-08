2be1e9230b51f5e0a75928c1979f8e7daf4fb221
YuutaTsubasa <yuuta.tsubasa.knight@gmail.com>
2024-10-09 00:56:56 +0800

    Update covers.

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -226,7 +226,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "e8rl31"
+  version_hash: "1p380qi"
 };
 async function get_hooks() {
   return {};
